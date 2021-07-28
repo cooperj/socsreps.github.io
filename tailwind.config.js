6ba7dd573606b0a76f84ea5860121da38cfb7226
William Oldham <wegg7250@gmail.com>
2021-07-28 21:58:50 +0100

    Misc styles and fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,15 +5,28 @@ module.exports = {
     ],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
+        extend: {
+            screens: {
+                'max-2xl': {'max': '1535px'},
+                'max-xl': {'max': '1279px'},
+                'max-lg': {'max': '1023px'},
+                'max-md': {'max': '767px'},
+                'max-sm': {'max': '639px'},
+            }
+        },
     },
     variants: {
+        ringColor: ['responsive', 'dark', 'focus-within', 'focus-visible', 'focus', 'hover'],
         extend: {
           borderColor: ['first'],
           borderOpacity:  ['first'],
           borderRadius:  ['first'],
           borderStyle:  ['first'],
           borderWidth:  ['first'],
+          scale: ['active'],
+          backgroundColor: ['active'],
+          ringWidth: ['focus-visible'],
+          ringOffsetWidth: ['focus-visible']
         }
     },
     plugins: [],
